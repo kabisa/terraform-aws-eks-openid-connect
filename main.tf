bbbcf6d56ee0c96448f639e3c6690bb9f8d12285
Sjuul Janssen <sjuul@outlook.com>
2020-10-22 14:29:49 +0200

    ignore thumbprint in lifecycle

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ data "external" "thumbprint" {
 }
 
 resource "aws_iam_openid_connect_provider" "openid_connect_provider" {
+  lifecycle {
+    ignore_changes = [data.external.thumbprint.result.thumbprint]
+  }
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.external.thumbprint.result.thumbprint]
   url             = var.cluster_oidc_issuer_url
